c2b5053faeebaa36eade51ac32e88664783774bc
Irene Scott <irenejinscott@gmail.com>
2020-06-19 22:29:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,18 +7,17 @@ module FormsLab
       erb :root
     end
 
-    get '/new' do
-      erb :'pirates/new'
-    end
+   get '/new' do
+     erb :'pirates/new'
+   end
 
-    post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
-
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
-      end
-      @ships = Ship.all
-
+   post '/pirates' do
+     @pirate = Pirate.new(params[:pirate])
+     
+     params[:pirate][:ships].each do |attributes|
+       Ship.new(attributes)
+     end
+     @ships = Ship.all
       erb :'pirates/show'
     end
 
