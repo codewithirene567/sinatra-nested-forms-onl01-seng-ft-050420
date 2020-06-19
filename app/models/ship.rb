73af7d9eb865f19ec46eb1edf9d4ae3eef4473f3
Irene Scott <irenejinscott@gmail.com>
2020-06-19 22:28:27 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -4,9 +4,9 @@ class Ship
 
   @@SHIPS = []
   def initialize(params)
-    @name = name
-    @type = type
-    @booty = booty
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty}
     @@SHIPS << self
   end
 
